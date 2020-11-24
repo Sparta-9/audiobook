bd941433c18b33a5a38355ffe02720860b3ff7ec
vikram singh <58397405+Sparta-9@users.noreply.github.com>
2020-11-24 21:53:15 +0530

    Update audiobook.py

--- audiobook.py
+++ audiobook.py
@@ -1,12 +1,12 @@
 import pypdf2
 import pyttsx3
-book=open("ops.pdf","rb")
+book=open("hacking.pdf","rb")
 pdfReader = pypdf2.pdfFileReader(book)
 pageg = pdfreader.numPages
 print(pages)
 speaker = pyttsx3.init()
 page = pdfReader.getpage(7)
-for num in range(7,pages):
+for num in range(15,pages):
     page = pdfReader.getpage(7)
     text = page.extractText()
     speaker.say(text)
